0f7b475bee3a20a219e608386681a1ec37bcc532
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-03-13 14:26:04 +0100

    Added .cctors of QFormats

--- Cuni.Arithmetics.FixedPoint/Formats.cs
+++ Cuni.Arithmetics.FixedPoint/Formats.cs
@@ -8,19 +8,28 @@ namespace Cuni.Arithmetics.FixedPoint
 {
     public abstract class QFormat
     {
-
+        public static int FractionalBitsCount { get; protected set; }
     }
 
     public class Q24_8 : QFormat
     {
-
+        static Q24_8()
+        {
+            FractionalBitsCount = 8;
+        }
     }
     public class Q16_16 : QFormat
     {
-
+        static Q16_16()
+        {
+            FractionalBitsCount = 16;
+        }
     }
     public class Q8_24 : QFormat
     {
-
+        static Q8_24()
+        {
+            FractionalBitsCount = 24;
+        }
     }
 }
