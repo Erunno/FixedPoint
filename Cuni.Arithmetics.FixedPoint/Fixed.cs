bb189dffcffb8eb7a6ef0e20be43753f7ed9f524
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-03-13 14:16:11 +0100

    Initial version

---
+++ Cuni.Arithmetics.FixedPoint/Fixed.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuni.Arithmetics.FixedPoint
+{
+    public struct Fixed<T> where T : QFormat
+    {
+        public Fixed(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Fixed<T> Add(Fixed<T> num)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Fixed<T> Subtract(Fixed<T> num)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Fixed<T> Multiply(Fixed<T> num)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Fixed<T> Divide(Fixed<T> num)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ Cuni.Arithmetics.FixedPoint/Formats.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuni.Arithmetics.FixedPoint
+{
+    public abstract class QFormat
+    {
+
+    }
+
+    public class Q24_8 : QFormat
+    {
+
+    }
+    public class Q16_16 : QFormat
+    {
+
+    }
+    public class Q8_24 : QFormat
+    {
+
+    }
+}
